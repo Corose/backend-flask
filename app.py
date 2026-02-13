7ca7ad28dc3fec1c02f936fca25f3eb3e115de88
Luis Angel Almaraz Angeles <luis.laaa69@gmail.com>
2026-02-13 02:16:10 -0600

    Connect PostgreSQL Railway

--- app.py
+++ app.py
@@ -1,21 +1,21 @@
+import os
 from flask import Flask
 from extensions import db
-from routes import api
+from routes import routes
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///okai.db"
+database_url = os.getenv("DATABASE_URL")
+
+if database_url:
+    # Fix para SQLAlchemy con postgres
+    database_url = database_url.replace("postgres://", "postgresql://")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = database_url or "sqlite:///okai.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db.init_app(app)
-app.register_blueprint(api, url_prefix="/api")
+app.register_blueprint(routes)
 
 with app.app_context():
     db.create_all()
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
-
-@app.route("/")
-def home():
-    return {"status": "ok", "service": "backend-flask"}
